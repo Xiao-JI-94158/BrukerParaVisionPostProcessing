0ee9321ef4718764227bdb6fd0d1e04b9c210b5a
xiao.ji.94158 <xiao.ji@ucsf.edu>
2023-03-21 00:12:31 -0700

    On branch main Changes to be committed: 	new file:   BrukerSpSpEpi.py

---
+++ BrukerSpSpEpi.py
@@ -0,0 +1,21 @@
+# Official packages
+import os
+
+# Third-party packages
+import numpy as np
+
+#In-house packages
+
+
+class BrukerSpSpEpi(object):
+
+    
+    def __init__(self) -> None:
+        raise NotImplementedError
+
+
+
+
+
+#    @property
+
