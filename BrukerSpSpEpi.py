e239b89ac1e250656ad2a42407083922be03d2ee
xiao.ji.94158 <xiao.ji@ucsf.edu>
2023-03-21 19:27:28 -0700

    On branch main Changes to be committed: 	new file:   BrukerSpSpEpi.py

--- BrukerSpSpEpi.py
+++ BrukerSpSpEpi.py
@@ -8,11 +8,25 @@ import numpy as np
 
 
 class BrukerSpSpEpi(object):
+    """
+        Basic Class that performs post-processing on EPI data acquired from Spectral-Spatial Selective Excitation (SpSp_EPI)
+    """
 
     
-    def __init__(self) -> None:
-        raise NotImplementedError
+    def __init__(self, exp_path) -> None:
+        """
+            Constructor of the class
 
+
+            param : exp_path, the path of dataset of the desired experiment
+            return : None
+            raise : 
+        """
+        raise NotImplementedError
+        raise FileNotFoundError
+        raise IsADirectoryError
+        raise 
+        pass
 
 
 
