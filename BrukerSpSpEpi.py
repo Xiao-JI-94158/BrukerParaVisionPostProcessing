f79aa062086bb8d3547cb5efa82d26baae69434b
Xiao JI - UCSF <xiao.ji@ucsf.edu>
2023-03-22 17:39:25 -0700

    Changes to be committed: 	modified:   BrukerSpSpEpi.py         Added:             _update_param_dict             _parse_array             _parse_single_value 	modified:   TestBrukerSpSpEpi.py         Added:             read_param_dict             parse_array             parse_single_value

--- BrukerSpSpEpi.py
+++ BrukerSpSpEpi.py
@@ -67,11 +67,90 @@ class BrukerSpSpEpi(object):
 
     def _update_param_dicts(self)->None:
         """
-        
+        Read a Bruker MRI experiment's method or acqp file to a
+        dictionary.
         """
-        raise NotImplementedError
+        param_dict = {}
+
+        with open(filepath, "r") as f:
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+
+                # when line contains parameter
+                if line.startswith('##$'):
+
+                    (param_name, current_line) = line[3:].split('=') # split at "="
+
+                    # if current entry (current_line) is arraysize
+                    if current_line[0:2] == "( " and current_line[-3:-1] == " )":
+                        value = _parse_array(f, current_line)
+
+                    # if current entry (current_line) is struct/list
+                    elif current_line[0] == "(" and current_line[-3:-1] != " )":
+
+                        # if neccessary read in multiple lines
+                        while current_line[-2] != ")":
+                            current_line = current_line[0:-1] + f.readline()
+
+                        # parse the values to a list
+                        value = [_parse_single_value(x) for x in current_line[1:-2].split(', ')]
+
+                    # otherwise current entry must be single string or number
+                    else:
+                        value = _parse_single_value(current_line)
+
+                    # save parsed value to dict
+                    param_dict[param_name] = value
+
+        return param_dict
         
-    
+
+    def _parse_array(current_file, line):
+
+        # extract the arraysize and convert it to numpy
+        line = line[1:-2].replace(" ", "").split(",")
+        arraysize = np.array([int(x) for x in line])
+
+        # then extract the next line
+        vallist = current_file.readline().split()
+
+        # if the line was a string, then return it directly
+        try:
+            float(vallist[0])
+        except ValueError:
+            return " ".join(vallist)
+
+        # include potentially multiple lines
+        while len(vallist) != np.prod(arraysize):
+            vallist = vallist + current_file.readline().split()
+
+        # try converting to int, if error, then to float
+        try:
+            vallist = [int(x) for x in vallist]
+        except ValueError:
+            vallist = [float(x) for x in vallist]
+
+        # convert to numpy array
+        if len(vallist) > 1:
+            return np.reshape(np.array(vallist), arraysize)
+        # or to plain number
+        else:
+            return vallist[0]
+
+    def _parse_single_value(val):
+
+        try: # check if int
+            result = int(val)
+        except ValueError:
+            try: # then check if float
+                result = float(val)
+            except ValueError:
+                # if not, should  be string. Remove  newline character.
+                result = val.rstrip('\n')
+
+        return result    
 
     
     def __init__(self, exp_path) -> None:
--- TestBrukerSpSpEpi.py
+++ TestBrukerSpSpEpi.py
@@ -1,6 +1,6 @@
 import os 
 
-
+import numpy as np
 
 
 def validate_data_paths(dataset_path, exp_nbr):
@@ -28,6 +28,93 @@ def validate_data_paths(dataset_path, exp_nbr):
     return paths_dict
 
 
+def read_param_file(filepath):
+    """
+    Read a Bruker MRI experiment's method or acqp file to a
+    dictionary.
+    """
+    param_dict = {}
+
+    with open(filepath, "r") as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+
+            # when line contains parameter
+            if line.startswith('##$'):
+
+                (param_name, current_line) = line[3:].split('=') # split at "="
+
+                # if current entry (current_line) is arraysize
+                if current_line[0:2] == "( " and current_line[-3:-1] == " )":
+                    value = parse_array(f, current_line)
+
+                # if current entry (current_line) is struct/list
+                elif current_line[0] == "(" and current_line[-3:-1] != " )":
+
+                    # if neccessary read in multiple lines
+                    while current_line[-2] != ")":
+                        current_line = current_line[0:-1] + f.readline()
+
+                    # parse the values to a list
+                    value = [parse_single_value(x)
+                             for x in current_line[1:-2].split(', ')]
+
+                # otherwise current entry must be single string or number
+                else:
+                    value = parse_single_value(current_line)
+
+                # save parsed value to dict
+                param_dict[param_name] = value
+
+    return param_dict
+
+
+def parse_array(current_file, line):
+
+    # extract the arraysize and convert it to numpy
+    line = line[1:-2].replace(" ", "").split(",")
+    arraysize = np.array([int(x) for x in line])
+
+    # then extract the next line
+    vallist = current_file.readline().split()
+
+    # if the line was a string, then return it directly
+    try:
+        float(vallist[0])
+    except ValueError:
+        return " ".join(vallist)
+
+    # include potentially multiple lines
+    while len(vallist) != np.prod(arraysize):
+        vallist = vallist + current_file.readline().split()
+
+    # try converting to int, if error, then to float
+    try:
+        vallist = [int(x) for x in vallist]
+    except ValueError:
+        vallist = [float(x) for x in vallist]
+
+    # convert to numpy array
+    if len(vallist) > 1:
+        return np.reshape(np.array(vallist), arraysize)
+    # or to plain number
+    else:
+        return vallist[0]
+
+def parse_single_value(val):
+
+    try: # check if int
+        result = int(val)
+    except ValueError:
+        try: # then check if float
+            result = float(val)
+        except ValueError:
+            # if not, should  be string. Remove  newline character.
+            result = val.rstrip('\n')
+
+    return result
 
  
     
@@ -41,5 +128,7 @@ if __name__ == '__main__':
 
 
     paths_dict = validate_data_paths(dataset_path, exp_nbr)
+    method_dict = read_param_file(paths_dict['method_path'])
+    print(method_dict)
     
         
