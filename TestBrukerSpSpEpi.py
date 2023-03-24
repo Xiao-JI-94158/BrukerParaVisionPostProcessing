a08e62ce4bae89c3569dc45c750fc9d54e409670
xiao.ji.94158 <xiao.ji@ucsf.edu>
2023-03-24 12:04:38 -0700

    Changes to be committed: 	new file:   .vscode/launch.json 	modified:   TestBrukerSpSpEpi.py         Rewrite UnitTest 	new file:   __pycache__/BrukerSpSpEpi.cpython-39.pyc

--- TestBrukerSpSpEpi.py
+++ TestBrukerSpSpEpi.py
@@ -11,24 +11,29 @@ from BrukerSpSpEpi import BrukerSpSpEpiExp
 
 class TestBrukerSpSpEpi(unittest.TestCase):
 
-    def test_exp_path(self):
-        exp_dir = "../20230305_163320_AgroseCylinder2_1_1"
-        exp_nbr = 21
+    def setUp(self) -> None:
+        self.exp_dir = "../20230305_163320_AgroseCylinder2_1_1"
+        self.exp_nbr = 21
         
-        exp_21 = BrukerSpSpEpiExp(exp_dir, exp_nbr)
+        self.spsp_epi = BrukerSpSpEpiExp(self.exp_dir, self.exp_nbr)
 
-        self.assertEqual(exp_21.exp_path , os.path.join(exp_dir, str(exp_nbr)))
+    def test_exp_path(self):
+        self.assertEqual(self.spsp_epi.exp_path , os.path.join(self.exp_dir, str(self.exp_nbr)))
 
     def test_update_data_paths(self):
-        exp_dir = "../20230305_163320_AgroseCylinder2_1_1"
-        exp_nbr = 21
-        
-        exp_21 = BrukerSpSpEpiExp(exp_dir, exp_nbr)
-
-        self.assertEqual(exp_21.data_paths_dict['fid_path'] , os.path.join(exp_dir, str(exp_nbr), "fid"))
-        self.assertEqual(exp_21.data_paths_dict['method_path'] , os.path.join(exp_dir, str(exp_nbr), "method"))
-        self.assertEqual(exp_21.data_paths_dict['acqp_path'] , os.path.join(exp_dir, str(exp_nbr), "acqp"))
+        self.assertEqual(self.spsp_epi.data_paths_dict['fid_path'] , os.path.join(self.spsp_epi.exp_path, "fid"))
+        self.assertEqual(self.spsp_epi.data_paths_dict['method_path'] , os.path.join(self.spsp_epi.exp_path, "method"))
+        self.assertEqual(self.spsp_epi.data_paths_dict['acqp_path'] , os.path.join(self.spsp_epi.exp_path, "acqp"))
        
+    def test_read_param_dicts(self):
+        self.assertEqual(self.spsp_epi.method_dict['Method'] , "<User:xji_spsp_epi>")
+        self.assertEqual(self.spsp_epi.method_dict['PVM_NAverages'] , 256)
+        self.assertTrue(np.array_equal(self.spsp_epi.method_dict['PVM_Matrix'], np.array([120,120]), equal_nan=True))
+
+        PVM_EncSteps1_array = np.linspace(start=-20, stop=59, num=80, endpoint=True, dtype=float)
+        self.assertTrue(np.array_equal(self.spsp_epi.method_dict['PVM_EncSteps1'], PVM_EncSteps1_array, equal_nan=True))
+        self.assertAlmostEqual(self.spsp_epi.method_dict['PVM_EpiReadOddGrad'] , -0.0805918017183784)
+
 
 if __name__ == '__main__':
     unittest.main()
